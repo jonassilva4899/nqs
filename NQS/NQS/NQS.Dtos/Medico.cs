6c9acda44ba779fe7adfc153d4c41bcb987291f9
Gustavo Luna <luna.gustavo@gmail.com>
2020-12-12 22:34:30 -0300

    tela de cadastro medico e correcoes de layout

---
+++ NQS/NQS/NQS.Dtos/Medico.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Leega.Util;
+
+
+namespace Leega.Dtos
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Crm { get; set; }
+        public string Nome { get; set; }
+    }
+}
